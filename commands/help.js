4a940ffd5ca09a8160e46c4d4542df59bf716506
obamasecret <101513091+Xzvise@users.noreply.github.com>
2022-06-17 20:42:29 +0600

    Add files via upload

---
+++ commands/help.js
@@ -0,0 +1,22 @@
+const { MessageEmbed } = require("discord.js");
+
+exports.execute = async (client, message, args) => {
+    const embed = new MessageEmbed()
+        .setAuthor("Commands")
+        .setTitle("Hey! i'm Exon!! This server's bot! Heres all the commands! Enjoy!")
+        .setDescription(`Total Commands: ${client.commands.size}`)
+        .setColor("BLURPLE")
+        .setTimestamp()
+        .setThumbnail(client.user.displayAvatarURL)
+        .setFooter(message.author.tag, message.author.displayAvatarURL);
+    client.commands.forEach(cmd => {
+        embed.addField(`${cmd.help.name}`, `Aliases: ${cmd.help.aliases.join(", ") || "None"}\nUsage: \`${client.prefix}${cmd.help.usage}\``, true);
+    });
+    return message.channel.send(embed);
+}
+
+exports.help = {
+    name: "help",
+    aliases: ["h"],
+    usage: `help`
+}
