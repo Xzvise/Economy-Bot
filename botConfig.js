be2ae13cfbc045a16c1ddfe053621620737f4b46
obamasecret <101513091+Xzvise@users.noreply.github.com>
2022-06-17 20:44:48 +0600

    Add files via upload

---
+++ botConfig.js
@@ -0,0 +1,8 @@
+module.exports = {
+    prefix: "e!",
+    admins: [
+        "730045222427426817"
+],
+    debug: false,
+    Channel: "907069273535545354"
+};
---
+++ index.js
@@ -0,0 +1,62 @@
+const Discord = require("discord.js");
+ const client = new Discord.Client({ disableMentions: 'everyone' });
+const Eco = require("quick.eco");
+client.eco = new Eco.Manager(); // quick.eco
+client.db = Eco.db; // quick.db
+client.config = require("./botConfig");
+client.commands = new Discord.Collection();
+client.aliases = new Discord.Collection();
+client.shop = {
+  laptop: {
+    cost: 1000
+  },
+  mobile: {
+    cost: 500
+  },
+  pc: {
+    cost: 2500
+  }
+};
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Ok')
+})
+
+app.listen(port, () => {
+  console.log(`Your app is listening at port ${port}`)
+})
+
+
+const fs = require("fs");
+
+fs.readdir("./events/", (err, files) => {
+    if (err) return console.error(err);
+    files.forEach(f => {
+        if (!f.endsWith(".js")) return;
+        const event = require(`./events/${f}`);
+        let eventName = f.split(".")[0];
+        client.on(eventName, event.bind(null, client));
+    });
+});
+
+fs.readdir("./commands/", (err, files) => {
+    if (err) return console.error(err);
+    files.forEach(f => {
+        if (!f.endsWith(".js")) return;
+        let command = require(`./commands/${f}`);
+        client.commands.set(command.help.name, command);
+        command.help.aliases.forEach(alias => {
+            client.aliases.set(alias, command.help.name);
+        });
+    });
+});
+
+
+client.login(process.env.token);
+
+
+//You can use this code with no credit needed :D
